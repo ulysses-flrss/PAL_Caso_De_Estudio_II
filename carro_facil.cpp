c4317e1a1aab8fb5a0a435c32a585a53f58d53f5
ulysses-flrss <ulisesf3136@gmail.com>
2022-04-04 12:10:04 -0600

    update

---
+++ carro_facil.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    setlocale(LC_ALL, "spanish");
+    int anio;
+    string agenciaNombre[3] = {"Perisur", "Valle", "Lomas"};
+    float ventas[4][3];
+
+    cout<<agenciaNombre[0]<<endl;
+    cout<<agenciaNombre[1]<<endl;
+    cout<<agenciaNombre[2]<<endl;
+
+    
+}
