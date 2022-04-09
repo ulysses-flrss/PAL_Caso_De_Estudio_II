d345ffc22f3ad074a37dac798d5ad6e771e2ef6c
ulysses-flrss <ulisesf3136@gmail.com>
2022-04-09 00:40:55 -0600

    update

--- carro_facil.cpp
+++ carro_facil.cpp
@@ -30,8 +30,10 @@ using namespace std;
 //Declaracion de funciones
     void validacionOpciones (int opcionElegida, int primeraOpcion, int ultimaOpcion);
     bool isWindows ();
-    void registrarVentas(double ventas[][3], int, int, string agencias[3], int);
+    void registrarVentas(/*double ventas[][3], int, int, char agencias[4], int*/);
     void verRegistro ();
+    void variables();
+    void menu();
     
 //--------------------------
 
@@ -52,7 +54,7 @@ struct Agencia {
     
     // métodos
     void ingresarRegistro(char nombreA[]){
-        nombre = nombreA; //No sé xq sale así el nombre xD
+        //nombre = nombreA; //No sé xq sale así el nombre xD
         cout << "Ingrese en dólares, las ventas totales del primer trimestre del año: $";
         cin >> ventasT1;
         cout << "Ingrese en dólares, las ventas totales del segundo trimestre del año: $";
@@ -65,21 +67,25 @@ struct Agencia {
 };
 
 int main () {
-    bool existen = false;
     setlocale(LC_ALL, "spanish");
-        const int NFILAS = 4;
-        const int NCOL = 3;
-        const int nspace = 3;
-        string agencias[nspace] = {"Perisur", "Valle", "Lomas"};
-        double ventas[NFILAS][NCOL];
-        int menuOpc;
-    
+	variables();
+    menu();
+}
 
+void variables () {
+    bool existen = false;
+    const int NFILAS = 4;
+    const int NCOL = 3;
+    const int nspace = 3;
+    //double ventas[NFILAS][NCOL];
+    int menuOpc;
+}
 
+void menu () {
+    int menuOpc;
+    const int nspace = 4;
     
-    
-
-	do {
+    do {
         cout<<"\nBienvenido al sistema de Carro Facil.\n";
 		cout<<"1- Ingresar Registros de Ventas\n";
 		cout<<"2- Ver Registro de Ventas\n";
@@ -93,45 +99,49 @@ int main () {
             break;
         } else {
             validacionOpciones(menuOpc, 1,3);
-			if (menuOpc == 1) registrarVentas(ventas, NFILAS, NCOL, agencias, nspace);
+			if (menuOpc == 1) registrarVentas();
             else if (menuOpc == 2) verRegistro();
         }
     } while (menuOpc < 1 || menuOpc > 3);
 }
 
-void registrarVentas(double ventas[][3], int nfilas, int ncol, string agencias[3], int nspace){
+void registrarVentas(/*double ventas[][3], int nfilas, int ncol, char agencias[3], int nspace*/){
 	int opc;
 	do{
-<<<<<<< Updated upstream
-		cout << "�En qu� agencia desea ingresar los datos?\n" << endl;
-=======
-		cout << "�En qu� agencia desea ingresar los datos?" << endl;
->>>>>>> Stashed changes
-		cout << "1. " << agencias[0] << endl;
-		cout << "2. " << agencias[1] << endl;
-		cout << "3. " << agencias[2] << endl;
-		cout << "4. Regresar" << endl;
-		cout << "Ingrese la opci�n que desee" << endl;
+		cout << "?En qu? agencia desea ingresar los datos?\n" << endl;
+		cout << "1.  Agencia Perisur" << endl;
+		cout << "2.  Agencia Valle" << endl;
+		cout << "3.  Agencia Lomas" << endl;
+		cout << "4. Regresar al menú anterior" << endl;
+		cout << "Ingrese la opci?n que desee" << endl;
 		cin >> opc;
 		
 		if(!(opc)){
 			validacionOpciones(opc, 1, 4);
 		}
 		switch (opc){
-			case 1:
-				cout << agencias[0];
+			case 1: {
+				Agencia perisur;
+				cout << "Ingrese las ventas totales (en dólares) del Trimestre 1:\n";cin>>perisur.ventasT1;
+				cout << "\n\nVentas totales: "<<perisur.ventasT1;
+
+            }
 			break;
 			
-			case 2:
-				cout << agencias[1];
+			case 2: {
+				Agencia valle;
+            }
 			break;
 			
-			case 3:
-				cout << agencias[2];
+			case 3: {
+				Agencia lomas;
+
+            }
 			break;
 			
-			case 4:
+			case 4: {
 				main();
+            }
 			break;
 		}
 	}while (opc < 1 || opc > 4);
@@ -147,7 +157,7 @@ void validacionOpciones (int opcionElegida, int primeraOpcion, int ultimaOpcion)
         cin.clear();
         cin.ignore();
         cout<<"ERROR: OPCION INVALIDA \n";
-        cout<<"Presione ENTER e int�ntelo de Nuevo...\n";
+        cout<<"Presione ENTER e int?ntelo de Nuevo...\n";
                 
         cin.ignore();
         if (isWindows()) {
@@ -168,7 +178,7 @@ bool isWindows () {
     }
 }
 void respuestas(){
- cout << "¿Cuál fue el total anual de ventas de la agencia Lomas?\n\n";
+        cout << "¿Cuál fue el total anual de ventas de la agencia Lomas?\n\n";
         cout << "¿Cuál fue el promedio de ventas de Carro Fácil en el segundo trimestre del año?\n\n";
         cout << "¿Cuáles agencias superaron el promedio de ventas del tercer trimestre?\n\n";
         cout << "¿En qué trimestre se registraron las menores ventas del año, considerando a todas las agencias?\n\n";
